4ff1d819538a95ce5be15e5ea6594a2be461a786
Alessandro Benati <85116900+a-benati@users.noreply.github.com>
2025-04-12 10:34:26 +0200

    Update setup.py

--- lib_rm_tools/setup.py
+++ lib_rm_tools/setup.py
@@ -2,27 +2,28 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
+VERSION = '1.3.1'
+
 include_gsl_dir = "/usr/include/"
 lib_gsl_dir = "/usr/lib/"
 
-VERSION = '1.3.1'
-
-ext = Extension(
-    "grid_tools",
-    sources=["grid_tools.pyx"],
-    include_dirs=[numpy.get_include(), include_gsl_dir],
-    library_dirs=[lib_gsl_dir],
-    libraries=["gsl", "gslcblas"]
-)
+extensions = [
+    Extension(
+        "grid_tools",
+        sources=["grid_tools.pyx"],
+        include_dirs=[numpy.get_include(), include_gsl_dir],
+        library_dirs=[lib_gsl_dir],
+        libraries=["gsl", "gslcblas"],
+    )
+]
 
 setup(
     name="rm_tools",
     version=VERSION,
-    description="rm_tools package from pyrmsynth",
     author="Michael Bell",
     author_email="mrbell@mpa-garching.mpg.de",
+    description="rm_tools package from pyrmsynth",
     packages=["rm_tools"],
     package_dir={"rm_tools": ""},
-    ext_modules=cythonize([ext], language_level="3"),
-    zip_safe=False
+    ext_modules=cythonize(extensions, language_level="3"),
 )
